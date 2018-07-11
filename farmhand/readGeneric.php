efd316d520bacfa50f6ed2803a537ef0c899bd3e
John Tokarz <jacktokarz@gmail.com>
2018-07-11 18:43:28 -0500

    got the lobby working better. added an empty match page

--- farmhand/readGeneric.php
+++ farmhand/readGeneric.php
@@ -1,23 +1,24 @@
 <?php
 
-$serverIP = $_SERVER['REMOTE_ADDR'];
+require_once 'config.php';
+$db = new MySQLi(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if ( $serverIP == '::1' ){
-	$db = new mysqli('localhost', 'root', '', 'data');
-} else {
-	$db = new mysqli('localhost', 'otterowl', '12@1Jack', 'otterowl_farmHand');
-}
 
 $query= $_POST['query'];
-
 $results = mysqli_query($db, $query);
-$array = array();
-while ($row = $results->fetch_array(MYSQL_ASSOC))
-{	
-	array_push($array, $row);
-}
-$final = json_encode($array);
 
-echo $final;
+if($results == '') {
+	echo $results;
+}
+else {
+	$array = array();
+	while ($row = $results->fetch_array(MYSQL_ASSOC))
+	{	
+		array_push($array, $row);
+	}
+	$final = json_encode($array);
+
+	echo $final;
+}
 
 ?>
