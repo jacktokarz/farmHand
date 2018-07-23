8cd034383b349e3b5f67bb1a23b7ee9c67cd81ac
John Tokarz <jacktokarz@gmail.com>
2018-07-23 18:38:09 -0500

    got it working, removed GameBox

--- farmhand/app/src/components/index.js
+++ farmhand/app/src/components/index.js
@@ -1,5 +1,5 @@
 
+export {default as Header} from './Header'
+export {default as Home} from './Home'
 export {default as Lobby} from './Lobby'
 export {default as Match} from './Match'
-export {default as Home} from './Home'
-export {default as Header} from './Header'
